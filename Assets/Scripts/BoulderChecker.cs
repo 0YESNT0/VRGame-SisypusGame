c682a29820813f5b155adb4bde8ff5caf95de01c
SchwarzDrachen <redgrowth41@gmail.com>
2025-07-17 18:19:30 +0800

    Boulder Check Initialization & changed level to updated version
    Added boulder checker when the boulder reaches the top and changed the lvl to the current design of it.

---
+++ Assets/Scripts/BoulderChecker.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderChecker : MonoBehaviour
+{
+    [SerializeField] public GameObject boulder;
+    [SerializeField] public float touchTimer = 5.0f;
+    private Collider boulderCollider;
+    public void Touchcheck()
+    {
+        boulderCollider = boulder.GetComponent<SphereCollider>();
+        if (boulderCollider.CompareTag("PlayerHands"))
+        {
+            touchTimer = 5.0f;
+            Debug.Log("Player Still Touching ball");
+        }
+        else
+        {
+            //Touch timer counts down and if done 
+            //door moves up and player wins
+        }
+    }
+}
