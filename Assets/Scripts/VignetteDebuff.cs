3b1a3f1c8701c4af0eb7def5ad7279a6ec6fcd91
SchwarzDrachen <redgrowth41@gmail.com>
2025-08-01 17:37:01 +0800

    XR Vignette Added
    Changed the vignette from post process to the XR toolkit's version

---
+++ Assets/Scripts/VignetteDebuff.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class VignetteDebuff : MonoBehaviour
+{
+    GameObject tunnelingVignette;
+    TunnelingVignetteController tunnelingVignetteController;
+    XROrigin xrOrigin;
+    [SerializeField] float vignetteIntensity = 0f;
+    [SerializeField] float durationOfVignette = 3.0f;
+
+    void Start()
+    {
+        xrOrigin = GameObject.Find("XR Origin (XR Rig)");
+        tunnelingVignette = GameObject.Find("TunnelingVignette");
+        tunnelingVignetteController = tunnelingVignette.GetComponent <TunnelingVignetteController >();
+        if (tunnelingVignetteController == null)
+        {
+            Debug.LogError("Tunneling Vignette not found");
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Collided with player");
+            StartCoroutine(VignetteEffect());
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator VignetteEffect()
+    {
+        tunnelingVignetteController.BeginTunnelingVignette();
+        Destroy(gameObject);
+        yield break;
+        
+    }
+}
